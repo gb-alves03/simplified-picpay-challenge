f44deeed33bfb483d0b3139de56f28da36805421
gbalves03 <gabrielhenriquealves321@gmail.com>
2024-06-20 12:44:53 -0300

    feat: Transaction entity creation

---
+++ src/main/java/br/com/simplifiedpicpay/domains/transaction/Transaction.java
@@ -0,0 +1,30 @@
+package br.com.simplifiedpicpay.domains.transaction;
+
+import br.com.simplifiedpicpay.domains.user.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity(name = "transactions")
+@Table(name = "Transaction")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private BigDecimal amount;
+    @ManyToOne
+    @JoinColumn(name = "sender_id")
+    private User sender;
+    @ManyToOne
+    @JoinColumn(name = "receiver_id")
+    private User receiver;
+    private LocalDateTime realizedAt;
+}
