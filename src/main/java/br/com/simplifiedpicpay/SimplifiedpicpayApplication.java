9420059d2b2ca5048ad821d22035ad9bfcff9c40
gbalves03 <gabrielhenriquealves321@gmail.com>
2024-06-20 12:43:27 -0300

    feat: project initial configuration

---
+++ src/main/java/br/com/simplifiedpicpay/SimplifiedpicpayApplication.java
@@ -0,0 +1,13 @@
+package br.com.simplifiedpicpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplifiedpicpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplifiedpicpayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/simplifiedpicpay/SimplifiedpicpayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.simplifiedpicpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplifiedpicpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
