37d8ec0aafbf18fd4ead533d06de662bf9778ccc
gbalves03 <gabrielhenriquealves321@gmail.com>
2024-06-24 14:32:36 -0300

    feat: creation of the users table on database using flyway migration

---
+++ src/main/resources/db/migration/V1__create-table-users.sql
@@ -0,0 +1,9 @@
+create table users(
+    id UUID primary key,
+    name varchar(255) not null,
+    document varchar(15) not null,
+    email varchar(255) not null,
+    password varchar(255) not null,
+    balance decimal not null,
+    user_type varchar(100) not null
+);
