a6326ec695d4e8e798ab07cffb5c375e2ea0b20e
gbalves03 <gabrielhenriquealves321@gmail.com>
2024-06-26 23:12:16 -0300

    feat: creation of the UserAlreadyExistsException class

---
+++ src/main/java/br/com/simplifiedpicpay/infra/exceptions/UserAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package br.com.simplifiedpicpay.infra.exceptions;
+
+public class UserAlreadyExistsException extends RuntimeException {
+    public UserAlreadyExistsException(String message) {
+        super(message);
+    }
+}
