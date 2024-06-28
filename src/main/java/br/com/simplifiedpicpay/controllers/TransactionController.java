e7f7b06bd73b28c862df88c811a2a7e972c52bcd
gbalves03 <gabrielhenriquealves321@gmail.com>
2024-06-28 11:53:57 -0300

    feat: findAll and findTransactionById methods implementation

--- src/main/java/br/com/simplifiedpicpay/controllers/TransactionController.java
+++ src/main/java/br/com/simplifiedpicpay/controllers/TransactionController.java
@@ -4,12 +4,12 @@ import br.com.simplifiedpicpay.domains.transaction.Transaction;
 import br.com.simplifiedpicpay.dtos.TransactionDtoRequest;
 import br.com.simplifiedpicpay.services.TransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/transactions")
@@ -23,5 +23,19 @@ public class TransactionController {
         Transaction transaction = this.transactionService.createTransaction(transactionDto);
 
         return new ResponseEntity<>(transaction, HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<Transaction>> getAllTransactions(@PageableDefault(size = 10, sort = {"realizedAt"}) Pageable pageable) {
+        var page = transactionService.findAllTransactions(pageable);
+
+        return ResponseEntity.ok(page);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Transaction> getTransactionById(@PathVariable("id") @RequestBody Long id) {
+        Transaction transaction = transactionService.findTransactionById(id);
+
+        return ResponseEntity.ok().body(transaction);
     }
 }
